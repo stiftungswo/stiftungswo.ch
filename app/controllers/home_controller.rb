449247a5c2e8664d613ff9403e56d8099cedc693
Lukas Bischof <polinderis@gmail.com>
2019-05-16 15:25:03 +0200

    Devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index
-    respond_to do |format|
-      format.html
-      format.json { render json: { blubb: 4 } }
-    end
-  end
+  def index; end
 end
